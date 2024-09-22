ed727c32d684bac9fb9a25827d6af56c5b7d9421
luca <lgabrielcostafarias@gmial.com>
2024-09-22 14:23:04 -0300

    added a view file and redered a template test

--- Sistema/Sistema/settings.py
+++ Sistema/Sistema/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Sistema.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_DIR = [BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
